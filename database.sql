2e26b0420d96e1496f704ba486652ece0e4722e9
Yenrenkhor <khoryenr@buffalo.edu>
2019-02-28 14:28:11 -0500

    updated database.sql

--- database.sql
+++ database.sql
@@ -3,22 +3,21 @@ ubid varchar(12) unique not null primary key,
 email varchar(50) unique not null,
 username varchar(50) unique not null,
 password varchar(50) not null,
-age int not null
 );
 
 create table items(
 item_id serial primary key,
-time_post timestamp on default current_timestamp,
+time_post timestamp default current_timestamp,
 description text,
 availability boolean not null default 't',
-post_by int(12) references ubid
+post_by int(12) references user_profile(ubid)
 );
 
 create table transaction(
 item int references items(item_id),
 from_user varchar(12) references user_profile(ubid),
 to_user varchar(12) references user_profile(ubid),
-transaction_date timestamp on default current_timestamp
+transaction_date timestamp default current_timestamp
 );
 
 --some random input data--
@@ -29,6 +28,11 @@ values ('50187324', 'something@buffalo.edu', 'something', 'randompassword', 21);
 insert into user_profile(ubid, email, username, password, age)
 values ('50217474', 'someoneelse@buffalo.edu', 'BuyEverything', 'anotherrandompassword', '20');
 
+
+insert into items(item_id, description, post_by)
+values (1, 'testing 123', '50166666');
+insert into items(item_id, description, post_by)
+values (2, 'test 123', '50187324');
 insert into items(description, post_by)
 values ('some 666666 item', '50166666');
 insert into items(description, post_by)
